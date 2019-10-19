66169f149b298671cdd17e40b9a1b6957f8afc82
Hamma2005 <Mohamed.benabedelfattah@esprit.tn>
2019-10-19 17:09:19 +0200

    Update Bill.java
    Commit test

--- pidev/pidev-ejb/src/main/java/Entities/Bill.java
+++ pidev/pidev-ejb/src/main/java/Entities/Bill.java
@@ -8,12 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 @Entity
 public class Bill  implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
- 
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	//
 }
-
-
